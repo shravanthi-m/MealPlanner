3ed9513bd771138e2cfe992e36fdea8f6961149a
charles <cmo@umass.edu>
2025-10-25 23:36:20 -0400

    revise meals so that it is a valid schema and change list of foods to refs

--- backend/models/meal.js
+++ backend/models/meal.js
@@ -3,9 +3,9 @@ import Food from "./food";
 
 const mealScehma = new mongoose.Schema({
   name: { type: String, required: true },
-  ingredients: [{ type: Schema.Types.ObjectId, required: false}],
+  ingredients: [{ type: Schema.Types.ObjectId, ref: 'Food', required: false}],
 //   info: { type: ?, required: true}
 });
 
-const Meal = mongoose.model("Food", mealScehma);
+const Meal = mongoose.model("Meal", mealScehma);
 export default Meal;
