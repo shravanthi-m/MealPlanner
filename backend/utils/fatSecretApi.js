72c9e6c2a7a4a4abdf2972109b8e84255d32a2c4
Charles Mo <cmo@umass.edu>
2025-11-24 15:55:57 -0500

    convert returned food from foods.search to object matching fields in Food schema

--- backend/utils/fatSecretApi.js
+++ backend/utils/fatSecretApi.js
@@ -54,17 +54,48 @@ export async function callFatSecretApi(accessToken, method, params = {}) {
 }
 
 /**
+ * @typedef {Object} V1Food
+ * @property {String} name
+ * @property {String} category
+ * @property {Array} ingredients
+ */
+/**
+ * 
+ * @param {Object} food 
+ * @param {String} food.food_name
+ * @param {String} food.food_type
+ * @returns {V1Food} converted food object
+ */
+function fsv1FoodtoFood(food) {
+    return {
+        name: food.food_name,
+        category: food.food_type,
+        ingredients: [], // FatSecret API does not provide ingredient details in search
+    };
+}
+
+/**
  * Search for food by expression using foods.search method in FatSecret API
  * @param {String} accessToken access token from 'getAccessToken' method
  * @param {String} searchExpression name of the food to search
  * @param {number} max_results maximum number of results to return
- * @returns {Promise<Object>} API response data
+ * @returns {Promise<Array<V1Food>>} API response data
  */
 export async function searchFoods(accessToken, searchExpression, max_results=10) {
+  // create params object  
   const params = {
     search_expression: searchExpression,
     max_results: max_results,
   };
+
+  // call foods.search method
   const data = await callFatSecretApi(accessToken, 'foods.search', params);
-  return data;
+
+  // get array of foods from response
+  let foods = Array.isArray(data.foods) ? data.foods : [data.foods];
+
+  // convert each food to our Food model format
+  foods = foods.map(fsv1FoodtoFood);
+
+  return foods;
 }
